b25a8fa4cfe81ee8f457486a8e2236e1e9523339
Lopez Hugo <hugo.lpz@gmail.com>
2023-10-25 11:00:33 +0200

    Update words.js

--- words.js
+++ words.js
@@ -1,1 +1,7 @@
-
+// words.js : I store your word-description pairs .
+// call : const words = require('./words.js'); 
+module.exports = {
+  "wasp":    { description: "One wasp eating a berry" },
+  "soldier": { description: "One cute soldier" },
+  "glass":   { description: "One blue glass of water with ice" },
+};
